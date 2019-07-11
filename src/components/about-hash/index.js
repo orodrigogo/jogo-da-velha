5924d671e24ae88e4419b5aefbf71639785ffe48
¨Cristiano <cristianomorgante@hotmail.com>
2019-07-11 11:05:43 -0300

    Desafio aula 047

---
+++ src/components/about-hash/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function AboutHash() {
+  return (
+    <div>
+      <nav className="about-hash">Sobre</nav>
+    </div>
+  );
+}
+
+export default AboutHash;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "./components/about-hash/style.css";
+import AboutHash from "./components/about-hash/index";
+
+ReactDOM.render(<AboutHash />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
