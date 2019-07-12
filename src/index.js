f16b8fd5ee98d7f046f21145778d04d22ab8f82e
¨Cristiano <cristianomorgante@hotmail.com>
2019-07-11 21:10:36 -0300

    Feito o link Sobre

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 
-import "./components/about-hash/style.css";
-import AboutHash from "./components/about-hash/index";
-
-ReactDOM.render(<AboutHash />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
