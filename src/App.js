3f899554cfcbe11184797fac28e7c358dd8b44b9
¨Cristiano <cristianomorgante@hotmail.com>
2019-07-12 17:07:44 -0300

    Desafio Intermediario aula 049

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Logo from "./objects/logo-hash";
 import AboutHash from "./objects/about-hash";
+import MenuHash from "./objects/menu-hash";
 
 function App() {
   return (
     <div>
       <Logo />
       <AboutHash />
+      <MenuHash />
     </div>
   );
 }
---
+++ src/objects/menu-hash/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+const MenuHash = () => (
+  <a href="#" className="menu-hash">
+    <span className="midlle">Menu</span>
+  </a>
+);
+
+export default MenuHash;
